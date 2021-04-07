4d8503b42e2340f3fab234d6c48f580f641866d0
almoustapha <doumbiaalmoustapha@gmail.com>
2021-04-07 16:49:15 +0000

    deploiement

--- djassa/settings.py
+++ djassa/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT  =  BASE_DIR/ 'static/'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
